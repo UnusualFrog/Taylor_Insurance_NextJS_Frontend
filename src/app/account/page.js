b2919e006e2c472af0d4a6793beb10941d03518a
Noah Forward <forwardnoah@gmail.com>
2025-02-23 14:38:23 -0330

    First commit

---
+++ src/app/account/page.js
@@ -0,0 +1,17 @@
+export default function Account() {
+    return (
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col items-center">
+
+        <div className="w-[500px] bg-white border rounded-md shadow-sm">
+          <h1 className="text-2xl font-bold text-gray-800 py-4 text-center">Account</h1>
+        </div>
+          
+        </main>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          
+        </footer>
+      </div>
+    );
+  }
+  
---
+++ src/app/get_quote/page.js
@@ -0,0 +1,16 @@
+export default function GetQuote() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col items-center">
+
+        <div className="w-[500px] bg-white border rounded-md shadow-sm">
+          <h1 className="text-2xl font-bold text-gray-800 py-4 text-center">Get Quote</h1>
+        </div>
+
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+      </footer>
+    </div>
+  );
+}
